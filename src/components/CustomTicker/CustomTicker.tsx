f916fe2d9d4f213e83f1ff66bfea39bfdae33a38
Aaron Acuña Alarcon <aacuna@medialabla.com>
2026-04-16 14:35:53 -0500

    fix

--- src/components/CustomTicker/CustomTicker.tsx
+++ src/components/CustomTicker/CustomTicker.tsx
@@ -35,7 +35,6 @@ export default function CustomTicker({
                 }}
             >
                 <span>{text}</span>
-                <span>{text}</span>
             </div>
         </div>
     )
