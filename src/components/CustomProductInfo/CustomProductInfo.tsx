1aee2f9146595c52d7a2da5157ba7782b8f14aba
Malu <frontend-8@medialabla.com>
2026-04-20 20:02:14 -0500

    fix

--- src/components/CustomProductInfo/CustomProductInfo.tsx
+++ src/components/CustomProductInfo/CustomProductInfo.tsx
@@ -3,17 +3,17 @@ import { usePDP } from "@faststore/core";
 import styles from "./CustomProductInfo.module.css";
 import { QuantitySelector } from "@faststore/ui";
 import "@faststore/ui/src/components/molecules/QuantitySelector/styles.scss";
-const [quantity, setQuantity] = useState(1);
 
 export default function CustomProductInfo(props: any) {
   const { data } = usePDP();
   const product = data?.product;
 
+  const [quantity, setQuantity] = useState(1);
+
   if (!product) return null;
 
   const price = product.offers?.offers?.[0]?.price;
 
-  // 🔥 FIX IMPORTANTE
   const config = props?.productTitle ?? props ?? {};
 
   const showRating = config.showRating;
@@ -23,6 +23,7 @@ export default function CustomProductInfo(props: any) {
   return (
     <div className={styles.info}>
       <h1>{product.name}</h1>
+
       {customSubtitle && (
         <div className={styles.subtitle}>{customSubtitle}</div>
       )}
@@ -30,27 +31,29 @@ export default function CustomProductInfo(props: any) {
       {showRating && (
         <div className={styles.rating}>⭐⭐⭐⭐☆</div>
       )}
+
       {showRef && (
         <div className={styles.ref}>
-         REF: {product.sku}
+          REF: {product.sku}
         </div>
       )}
-      <div className={styles.price}>S/ {price}</div>
-      <div className={styles.buyRow}>
-  
-   <div className={styles.container}>
-    <QuantitySelector
-        min={1}
-        max={10}
-        initial={1}
-        onChange={(value) => setQuantity(value)}
-    />
-   </div>
-    <div className={styles.buyButton}>
-        {props.children}
-    </div>
 
-    </div>
+      <div className={styles.price}>S/ {price}</div>
+
+      <div className={styles.buyRow}>
+        <div className={styles.container}>
+          <QuantitySelector
+            min={1}
+            max={10}
+            initial={1}
+            onChange={(value) => setQuantity(value)}
+          />
+        </div>
+
+        <div className={styles.buyButton}>
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 }
